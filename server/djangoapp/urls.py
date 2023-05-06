4253804a0004072bab38787ca0495cd5fe85b117
jetty53 <jeetnath2016@gmail.com>
2023-05-06 15:54:30 -0400

    Second Commit

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,20 +8,23 @@ urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-
+    path(route='', view=views.get_dealerships, name='index'),   
     # path for about view
-    # path(route='', view=views.about, name='about'),
+    path(route='', view=views.about, name='about'),
 
     # path for contact us view
     path(route='', view=views.contact, name='contact'),
 
     # path for registration
+    path(route='', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='', view=views.logout_request, name='logout'),
 
-    path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
